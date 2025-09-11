61559a3c1c3d8c91f32f257eb02e5b49343ac000
John9xDoe <felix9fox328forest@gmail.com>
2025-09-11 21:45:47 +0300

    feat: add square and triangle generation

--- synth_data_generator.py
+++ synth_data_generator.py
@@ -35,6 +35,14 @@ def generate_object(fig_type, h_bg=640, w_bg=640, color_fig=None, color_bg=None,
 
     if fig_type == 'circle':
         _generate_circle(bg, center, base_size, color_fig)
+    elif fig_type == 'rectangle':
+        _generate_square(bg, center, base_size, color_fig)
+    elif fig_type == 'triangle':
+        center = (
+            random.randint(int(np.sqrt(3) * base_size), int(w_bg - np.sqrt(3) * base_size)),
+            random.randint(base_size * 2, h_bg - base_size)
+        )
+        _generate_triangle(bg, center, base_size, color_fig)
 
     return bg
 
@@ -42,9 +50,34 @@ def _generate_circle(bg, center, radius, color_circle):
     cv2.circle(bg, center, radius, color_circle, thickness=-1)
     return bg
 
+def _generate_square(bg, center, apothem, color_square):
+    cx, cy = center
+    cv2.rectangle(
+        bg,
+        (cx - apothem, cy - apothem),
+        (cx + apothem, cy + apothem),
+        color_square,
+        thickness=-1
+    )
+    return bg
+
+def _generate_triangle(bg, center, apothem, color_triangle):
+    cx, cy = center
+    pts = [
+        (cx - int(np.sqrt(3) * apothem), cy + apothem),
+        (cx, cy - 2 * apothem),
+        (cx + int(np.sqrt(3) * apothem), cy + apothem)
+    ]
+
+    pts = np.array([pts], dtype=np.int32)
+
+    cv2.fillPoly(bg, [pts], color_triangle)
+    return bg
+
 def visualize(bg, time=0, name='Figure'):
     cv2.imshow(name, bg)
     cv2.waitKey(time)
+    cv2.destroyAllWindows()
 
 
-visualize(generate_object('circle',640, 640), time=3000)
+visualize(generate_object('triangle',640, 640), time=1000)
