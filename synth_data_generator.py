bd54efcf9381415958c976141b36f659830fae2d
John9xDoe <felix9fox328forest@gmail.com>
2025-09-11 20:21:08 +0300

    refactor: separation of shapes generation and visualization

--- synth_data_generator.py
+++ synth_data_generator.py
@@ -11,7 +11,7 @@ def generate_background(height=640, width=640, vis=True, color=None):
         cv2.waitKey(0)
     return bg
 
-def generate_object(h_bg=640, w_bg=640, color_fig=None, color_bg=None, radius=100):
+def generate_object(fig_type, h_bg=640, w_bg=640, color_fig=None, color_bg=None, base_size=100):
     bg = generate_background(h_bg, w_bg, vis=False, color=color_bg)
 
     if color_fig is None:
@@ -28,9 +28,23 @@ def generate_object(h_bg=640, w_bg=640, color_fig=None, color_bg=None, radius=10
             random.randint(0, 255)
         )
 
-    center = (random.randint(radius,h_bg - radius), random.randint(radius, w_bg - radius))
-    cv2.circle(bg, center, radius, color_fig,None, None, None)
-    cv2.imshow("Figure", bg)
-    cv2.waitKey(0)
+    center = (
+        random.randint(base_size,h_bg - base_size),
+        random.randint(base_size, w_bg - base_size)
+    )
 
-generate_object(640, 640)
+    if fig_type == 'circle':
+        _generate_circle(bg, center, base_size, color_fig)
+
+    return bg
+
+def _generate_circle(bg, center, radius, color_circle):
+    cv2.circle(bg, center, radius, color_circle, thickness=-1)
+    return bg
+
+def visualize(bg, time=0, name='Figure'):
+    cv2.imshow(name, bg)
+    cv2.waitKey(time)
+
+
+visualize(generate_object('circle',640, 640), time=3000)
